ba2251ce56c766c4cc081be1a233d152b60b693b
bee-joo <bukin-danila@yandex.ru>
2022-08-05 17:54:10 +0300

    Add id

--- src/main/java/com/example/storecatalog/dto/StoreDTO.java
+++ src/main/java/com/example/storecatalog/dto/StoreDTO.java
@@ -1,11 +1,13 @@
 package com.example.storecatalog.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
 public class StoreDTO {
-    private String name, description;
+    private String id, name, description;
     private List<AddressDTO> addresses;
 }
