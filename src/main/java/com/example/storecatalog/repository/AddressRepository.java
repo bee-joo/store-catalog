2c507427afa395908a1d566c9b40ff616dcd72d0
bee-joo <bukin-danila@yandex.ru>
2022-08-05 17:52:04 +0300

    Add findAddressByIdAndStoreId method

--- src/main/java/com/example/storecatalog/repository/AddressRepository.java
+++ src/main/java/com/example/storecatalog/repository/AddressRepository.java
@@ -4,8 +4,11 @@ import com.example.storecatalog.document.Address;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AddressRepository extends MongoRepository<Address, String> {
     List<Address> findAddressesByStoreId(String id);
     void deleteAddressesByStoreId(String id);
+
+    Optional<Address> findAddressByIdAndStoreId(String id, String storeId);
 }
