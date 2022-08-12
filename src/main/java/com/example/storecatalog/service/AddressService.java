cc8bc34a5e5b62e48ff42237abc31ff1d50cb310
bee-joo <bukin-danila@yandex.ru>
2022-08-12 20:50:14 +0300

    Throw exceptions

--- src/main/java/com/example/storecatalog/service/AddressService.java
+++ src/main/java/com/example/storecatalog/service/AddressService.java
@@ -2,46 +2,40 @@ package com.example.storecatalog.service;
 
 import com.example.storecatalog.document.Address;
 import com.example.storecatalog.document.Store;
+import com.example.storecatalog.exception.NotFoundException;
 import com.example.storecatalog.repository.AddressRepository;
 import com.example.storecatalog.repository.StoreRepository;
 import com.example.storecatalog.utils.AddressMapper;
 import com.example.storecatalog.utils.StoreMapper;
 import com.example.storecatalog.view.AddressView;
+import com.example.storecatalog.view.ResponseView;
 import com.example.storecatalog.view.StoreView;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class AddressService {
     private final AddressRepository addressRepository;
-    private StoreRepository storeRepository;
-    private AddressMapper addressMapper;
-    private StoreMapper storeMapper;
+    private final StoreRepository storeRepository;
 
-    public ResponseEntity<AddressView> findAddressById(String id) {
-        Optional<Address> address = addressRepository.findById(id);
+    public ResponseView<AddressView> findAddressById(String id) {
+        Address address = addressRepository.findById(id)
+                .orElseThrow(() ->
+                        new NotFoundException(String.format("Address with id %s not found", id)));
 
-        if (address.isPresent()) {
-            AddressView addressView = addressMapper.toView(address.get());
-            return new ResponseEntity<>(addressView, HttpStatus.OK);
-        }
-
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseView<>(AddressMapper.toView(address));
     }
 
-    public ResponseEntity<StoreView> findStoreByAddressId(String id) {
-        Optional<Address> address = addressRepository.findById(id);
+    public ResponseView<StoreView> findStoreByAddressId(String id) {
+        Address address = addressRepository.findById(id)
+                .orElseThrow(() ->
+                        new NotFoundException(String.format("Address with id %s not found", id)));
 
-        if (address.isPresent()) {
-            Store store = storeRepository.findById(address.get().getStoreId()).get();
-            return new ResponseEntity<>(storeMapper.toView(store), HttpStatus.OK);
-        }
+        Store store = storeRepository.findById(address.getStoreId())
+                .orElseThrow(() ->
+                        new NotFoundException(String.format("Store with id %s not found", id)));
 
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseView<>(StoreMapper.toView(store));
     }
 }
