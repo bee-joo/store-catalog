fe50c92b109d4d65313bb70be49c9d3f3a3a462c
bee-joo <bukin-danila@yandex.ru>
2022-08-12 20:52:28 +0300

    ResponseView as response body; throw exceptions

--- src/main/java/com/example/storecatalog/service/StoreService.java
+++ src/main/java/com/example/storecatalog/service/StoreService.java
@@ -4,19 +4,18 @@ import com.example.storecatalog.document.Address;
 import com.example.storecatalog.document.Store;
 import com.example.storecatalog.dto.AddressDTO;
 import com.example.storecatalog.dto.StoreDTO;
+import com.example.storecatalog.exception.NotFoundException;
 import com.example.storecatalog.repository.AddressRepository;
 import com.example.storecatalog.repository.StoreRepository;
 import com.example.storecatalog.utils.AddressMapper;
 import com.example.storecatalog.utils.StoreMapper;
 import com.example.storecatalog.view.AddressView;
+import com.example.storecatalog.view.ResponseView;
 import com.example.storecatalog.view.StoreView;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -25,102 +24,97 @@ import java.util.stream.Collectors;
 public class StoreService {
 
     private StoreRepository storeRepository;
-    private StoreMapper storeMapper;
     private AddressRepository addressRepository;
-    private AddressMapper addressMapper;
 
-    public Store addStore(StoreDTO store) {
-        Store newStore = storeMapper.toEntity(store);
+    public ResponseView<Store> addStore(StoreDTO store) {
+        Store newStore = storeRepository.save(StoreMapper.toEntity(store));
 
-        return storeRepository.save(newStore);
+        return new ResponseView<>(newStore);
     }
 
-    public ResponseEntity<StoreView> findStoreById(String id) {
-        Optional<Store> store = storeRepository.findById(id);
+    public ResponseView<StoreView> findStoreById(String id) {
+        Store store = storeRepository.findById(id)
+                .orElseThrow(() ->
+                        new NotFoundException(String.format("Store with id %s not found", id)));
 
-        if (store.isPresent()) {
-            StoreView storeView = storeMapper.toView(store.get());
-            return new ResponseEntity<>(storeView, HttpStatus.OK);
-        }
+        StoreView storeView = StoreMapper.toView(store);
 
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseView<>(storeView);
     }
 
-    public List<StoreView> findAllStores() {
-        return storeRepository.findAll()
+    public ResponseView<List<StoreView>> findAllStores() {
+        List<StoreView> storeViews = storeRepository.findAll()
                 .stream()
-                .map(it -> storeMapper.toView(it))
+                .map(StoreMapper::toView)
                 .toList();
+
+        return new ResponseView<>(storeViews);
     }
 
-    public List<AddressView> findAllAddressesById(String id) {
-        return addressRepository.findAddressesByStoreId(id)
+    public ResponseView<List<AddressView>> findAllAddressesById(String id) {
+        List<AddressView> addressViews = addressRepository.findAddressesByStoreId(id)
                 .stream()
-                .map(it -> addressMapper.toView(it))
+                .map(AddressMapper::toView)
                 .toList();
+
+        return new ResponseView<>(addressViews);
     }
 
-    public ResponseEntity<List<Address>> addAddressToStore(String id, Set<AddressDTO> addresses) {
-        Optional<Store> store = storeRepository.findById(id);
+    public ResponseView<List<Address>> addAddressToStore(String id, Set<AddressDTO> addresses) {
+        Store store = storeRepository.findById(id)
+                .orElseThrow(() ->
+                        new NotFoundException(String.format("Store with id %s not found", id)));
 
-        if (store.isPresent()) {
-            Set<Address> newAddresses = addresses
-                    .stream()
-                    .map(it -> {
-                        Address address = addressMapper.toEntity(it);
-                        address.setStoreId(id);
+        Set<Address> newAddresses = addresses
+                .stream()
+                .map(it -> {
+                    Address address = AddressMapper.toEntity(it);
+                    address.setStoreId(id);
 
-                        return address;
-                    })
-                    .collect(Collectors.toSet());
+                    return address;
+                })
+                .collect(Collectors.toSet());
 
-            List<Address> storedAddresses = addressRepository.saveAll(newAddresses);
-            Set<String> addressIds =
-                    storedAddresses.stream()
-                            .map(Address::getId)
-                            .collect(Collectors.toSet());
+        List<Address> storedAddresses = addressRepository.saveAll(newAddresses);
+        Set<String> addressIds = storedAddresses
+                .stream()
+                .map(Address::getId)
+                .collect(Collectors.toSet());
 
-            Store newStore = store.get();
-            if (newStore.getAddresses() == null) {
-                newStore.setAddresses(addressIds);
-            } else {
-                newStore.getAddresses().addAll(addressIds);
-            }
-
-            storeRepository.save(newStore);
-
-            return new ResponseEntity<>(storedAddresses, HttpStatus.CREATED);
+        if (store.getAddresses() == null) {
+            store.setAddresses(addressIds);
+        } else {
+            store.getAddresses().addAll(addressIds);
         }
 
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        storeRepository.save(store);
+
+        return new ResponseView<>(storedAddresses);
     }
 
-    public ResponseEntity<Store> updateStore(String id, StoreDTO storeDTO) {
-        Optional<Store> storeOptional = storeRepository.findById(id);
+    public ResponseView<Store> updateStore(String id, StoreDTO storeDTO) {
+        Store store = storeRepository.findById(id)
+                .orElseThrow(() ->
+                        new NotFoundException(String.format("Store with id %s not found", id)));
 
-        if (storeOptional.isPresent()) {
-            Store storeEntity = storeMapper.toEntity(storeDTO, storeOptional.get());
+        Store storeEntity = StoreMapper.updateEntity(storeDTO, store);
 
-            return new ResponseEntity<>(storeRepository.save(storeEntity), HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseView<>(storeRepository.save(storeEntity));
     }
 
-    public HttpStatus deleteStore(String id) {
+    public ResponseView<String> deleteStore(String id) {
         addressRepository.deleteAddressesByStoreId(id);
         storeRepository.deleteById(id);
 
-        return HttpStatus.OK;
+        return new ResponseView<>("Deleted");
     }
 
-    public ResponseEntity<Address> updateStoreAddress(String storeId, String addressId, AddressDTO addressDTO) {
-        Optional<Address> addressOptional = addressRepository.findAddressByIdAndStoreId(addressId, storeId);
+    public ResponseView<Address> updateStoreAddress(String storeId, String addressId, AddressDTO addressDTO) {
+        Address address = addressRepository.findAddressByIdAndStoreId(addressId, storeId)
+                .orElseThrow(() -> new NotFoundException("Not found"));
 
-        if (addressOptional.isPresent()) {
-            Address addressEntity = addressMapper.toEntity(addressDTO, addressOptional.get());
+        Address addressEntity = AddressMapper.updateEntity(addressDTO, address);
 
-            return new ResponseEntity<>(addressRepository.save(addressEntity), HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseView<>(addressRepository.save(addressEntity));
     }
 }
