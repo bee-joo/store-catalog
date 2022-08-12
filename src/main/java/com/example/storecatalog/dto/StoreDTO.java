9c1d904858f2206b4332d6e3fd4486e483821853
bee-joo <bukin-danila@yandex.ru>
2022-08-12 20:51:25 +0300

    DTO doesn't have list of address DTO

--- src/main/java/com/example/storecatalog/dto/StoreDTO.java
+++ src/main/java/com/example/storecatalog/dto/StoreDTO.java
@@ -3,11 +3,8 @@ package com.example.storecatalog.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 public class StoreDTO {
     private String id, name, description;
-    private List<AddressDTO> addresses;
 }
